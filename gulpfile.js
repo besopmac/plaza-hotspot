0fa922806ccb21b176bc22d070bf6e4dc881b9c4
Bruno Nigro <brunosopmac@gmail.com>
2017-04-19 19:38:34 -0300

    Atualizando as tasks do gulpfile.js #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp        = require('gulp')
 var pug         = require('gulp-pug')
 var stylus      = require('gulp-stylus')
-var livereload  = require('browser-sync')
+var browserSync = require('browser-sync').create()
 
 // Pug
 gulp.task('pug', function(){
@@ -11,9 +11,29 @@ gulp.task('pug', function(){
 })
 
 // Stylus
-
+gulp.task('stylus', function(){
+    return gulp.src('src/assets/css/*.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('./output/assets/css/'))
+})
 
 // Watch
+gulp.task('sentinel', ['pug', 'stylus'], function(done) {
+    browserSync.reload()
+    done()
+})
 
+// Serve
+gulp.task('serve', ['pug', 'stylus'], function() {
 
-// Server
+    // Serving files on the output folder
+    browserSync.init({
+        server: {
+            baseDir: './output'
+        }
+    })
+
+    // Reload the browser
+    gulp.watch('src/*.pug', ['sentinel'])
+    gulp.watch('src/assets/css/*.styl', ['sentinel'])
+})
